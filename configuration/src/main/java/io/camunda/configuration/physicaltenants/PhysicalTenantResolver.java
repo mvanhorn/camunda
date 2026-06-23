f4ea611fdf501466f35b1138224302c0240568a2
Konstantinos Mylosis <konstantinos.mylosis@camunda.com>
2026-06-23 17:23:29 +0300

    feat: documents bind root config and per physical tenant overlay

--- configuration/src/main/java/io/camunda/configuration/physicaltenants/PhysicalTenantResolver.java
+++ configuration/src/main/java/io/camunda/configuration/physicaltenants/PhysicalTenantResolver.java
@@ -96,6 +96,8 @@ public final class PhysicalTenantResolver implements PhysicalTenantIds {
       binder.bind(Camunda.PREFIX, Bindable.ofInstance(physicalTenant));
       binder.bind(
           PHYSICAL_TENANTS_PREFIX + "." + physicalTenantId, Bindable.ofInstance(physicalTenant));
+      physicalTenant.setDocument(
+          PhysicalTenantDocumentConfigurations.forPhysicalTenant(physicalTenantId, environment));
       resolvedPhysicalTenants.put(physicalTenantId, physicalTenant);
     }
     if (!resolvedPhysicalTenants.containsKey(PhysicalTenantIds.DEFAULT_PHYSICAL_TENANT_ID)) {
